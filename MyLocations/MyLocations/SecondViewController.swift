179e158fa5cb7c03a3e324829dc748c20bd6dcbe
Terry Griffin <terry.griffin@mwsu.edu>
2015-03-29 18:49:00 -0500

    Update SecondViewController.swift

--- MyLocations/MyLocations/SecondViewController.swift
+++ MyLocations/MyLocations/SecondViewController.swift
@@ -9,18 +9,10 @@
 import UIKit
 import MapKit
 
-class SecondViewController: UIViewController,MKMapViewDelegate {
+class SecondViewController: UIViewController {
 
     @IBOutlet weak var mapView: MKMapView!
     
-<<<<<<< HEAD
-    var currentPoint = PointData()
-    
-    var annotation = MKPointAnnotation()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-=======
     override func viewDidLoad() {
         super.viewDidLoad()
         // 1
@@ -40,43 +32,11 @@ class SecondViewController: UIViewController,MKMapViewDelegate {
         annotation.subtitle = "London"
         mapView.addAnnotation(annotation)
     }
->>>>>>> ec625be31fbfd996bfe91348d8eb544c8dd80789
 
-    }
-    
-    override func viewWillAppear(animated: Bool) {
-        var showLocation = NSTimer.scheduledTimerWithTimeInterval(2.0, target: self, selector: "updateMap", userInfo: nil, repeats: true)
-    }
-    
-    func updateMap(){
-        currentPoint = (self.tabBarController as CustomTabBarController).currentPoint
-        
-        
-        
-        let currentLocation = CLLocationCoordinate2D(
-            latitude: currentPoint.lat,
-            longitude: currentPoint.lon
-        )
-        
-        let span = MKCoordinateSpanMake(0.05, 0.05)
-        let region = MKCoordinateRegion(center: currentLocation, span: span)
-        mapView.setRegion(region, animated: true)
-        
-        
-        
-        annotation.setCoordinate(currentLocation)
-        //annotation.title = "Big Ben"
-        //annotation.subtitle = "London"
-        mapView.removeAnnotation(annotation)
-        mapView.addAnnotation(annotation)
-    }
-    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    
+
 
 }
-
